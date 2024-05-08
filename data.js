975bd30349f22d2a575c6cf3f5a1ddd53faae271
Praniti  Kubal <140830559+Praniti1594@users.noreply.github.com>
2024-05-08 21:33:35 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,47 @@
+export const data = [
+
+    {
+        question: "What is the term used to describe a star that has exhausted its nuclear fuel and collapsed to a very small and dense state?",
+        option1: "White dwarf",
+        option2: "Neutron star",
+        option3: "Black hole",
+        option4: "Protostar",
+        ans: 2,
+    },
+
+    {
+        question: "What is the process by which a star transforms into a red giant and sheds its outer layers",
+        option1: " Stellar collapse",
+        option2: "Stellar fusion",
+        option3: "Stellar evolution",
+        option4: " Stellar nucleosynthesis",
+        ans: 3,
+    },
+    {
+        question: "What is the term used to describe the region around a black hole from which nothing can escape, including light?",
+        option1: "Event horizon",
+        option2: "Singularity",
+        option3: "Photon sphere",
+        option4: " Ergosphere",
+        ans: 1,
+    },
+
+    {
+        question: "What is the process by which a star explodes in a violent burst, releasing an enormous amount of energy?",
+        option1: "Supernova",
+        option2: "Nova",
+        option3: "Hypernova",
+        option4: " Stellar flare",
+        ans: 1,
+    },
+
+    {
+        question: "What is the term used to describe a star that has exhausted its nuclear fuel and is no longer undergoing fusion reactions?",
+        option1: "White dwarf",
+        option2: "Neutron star",
+        option3: "Black hole",
+        option4: "Protostar",
+        ans: 1,
+    },
+
+]
